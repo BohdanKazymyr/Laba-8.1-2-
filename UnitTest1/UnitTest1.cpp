48cd2af9d2ea1a6157bb9f7996d9781a4fb36ead
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-01 16:21:54 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 8.1(rec)/Laba 8.1(rec).cpp"
+#pragma warning(disable : 4996)
+#define _CRT_SECURE_NO_WARNINGS
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101]{};
+			Count(str, 1);
+			char* dest1 = new char[151];
+			Change(dest1, str, dest1, 0);
+		}
+	};
+}
